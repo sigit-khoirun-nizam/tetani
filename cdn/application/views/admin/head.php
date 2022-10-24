9d5ae362cf9a2eb9bcaadc4051030bf63d350656
[sigit-khoirun-nizam] <[sigitkhoirunnizam10@gmail.com]>
2022-10-24 21:56:44 +0700

    Tambah Data Transporter

--- cdn/application/views/admin/head.php
+++ cdn/application/views/admin/head.php
@@ -16,6 +16,20 @@
 	<link rel="stylesheet" href="<?= base_url() ?>assets/css/minmin.css?v=<?= time() ?>">
 	<link rel="stylesheet" href="<?= base_url() ?>assets/css/util.css">
 
+    <!--My Custom-->
+    <!-- Datatables -->
+    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/js/all.min.js" integrity="sha512-RXf+QSDCUQs5uwRKaDoXt55jygZZm2V++WUZduaU/Ui/9EGp3f/2KZVahFZBKGH0s774sd3HmrhUy+SgOFQLVQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+    <script src="https://cdn.datatables.net/1.10.24/js/jquery.dataTables.min.js"></script>
+    <script src="https://cdn.datatables.net/1.10.24/js/dataTables.bootstrap4.min.js"></script>
+    <!--My Custom-->
+
 	<!-- IMPORTANT JS -->
 	<script src="<?= base_url() ?>assets/js/core/jquery-3.2.1.min.js"></script>
 	<?php if (isset($tiny) and $tiny == true) { ?>
@@ -116,7 +130,7 @@
 						<div class="collapse" id="transporter">
 							<ul class="">
 								<li>
-									<a href="<?= site_url("ngadimin") ?>">
+									<a href="<?= site_url("datatransporter/index") ?>">
 										<span class="">Data Transporter</span>
 									</a>
 								</li>
---
+++ cdn/application/views/datatransporter/add.php
@@ -0,0 +1,141 @@
+<div class="container pt-5">
+    <h3><?= $title ?></h3>
+    <nav aria-label="breadcrumb">
+        <ol class="breadcrumb ">
+            <li class="breadcrumb-item"><a>Data Transporter</a></li>
+            <li class="breadcrumb-item "><a href="<?= base_url('datatransporter/index'); ?>">List Data Transporter</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Tambah Data</li>
+        </ol>
+    </nav>
+    <div class="row">
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <?php
+                        //create form
+                        $attributes = array('id' => 'FrmAddMahasiswa', 'method' => "post", "autocomplete" => "off");
+                        echo form_open('', $attributes);
+                        ?>
+
+                        <div class="form-group row">
+                            <label for="nama" class="col-sm-2 col-form-label">Nama</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="nama" name="nama" value=" <?= set_value('nama'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('nama') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="no_hp" class="col-sm-2 col-form-label">No HP</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="no_hp" name="no_hp" value=" <?= set_value('no_hp'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('no_hp') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="email" class="col-sm-2 col-form-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="email" name="email" value=" <?= set_value('email'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('email') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="alamat" class="col-sm-2 col-form-label">Alamat</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="alamat" name="alamat" value=" <?= set_value('alamat'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('alamat') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="provinsi" class="col-sm-2 col-form-label">Provinsi</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="provinsi" name="provinsi" value=" <?= set_value('provinsi'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('provinsi') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="kota" class="col-sm-2 col-form-label">Kota</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="kota" name="kota" value=" <?= set_value('kota'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('kota') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                    <!--form-->
+                    </div>
+                </div>
+            </div>
+
+            <div class="col-md-6">
+                <div class="card">
+                    <div class="card-body">
+                        <?php
+                        //create form
+                        $attributes = array('id' => 'FrmAddMahasiswa', 'method' => "post", "autocomplete" => "off");
+                        echo form_open('', $attributes);
+                        ?>
+
+                        <div class="form-group row">
+                            <label for="status" class="col-sm-4 col-form-label">Status</label>
+                            <div class="col-sm-10">
+                                <select class="form-control" id="status" name="status">
+                                    <option value="Pilih" selected disabled>Pilih</option>
+                                    <option value="Aktif" <?php if (set_value('status') == "Aktif") : echo "selected";
+                                    endif; ?>>Aktif</option>
+                                    <option value="Tidak Aktif" <?php if (set_value('status') == "Tidak Aktif") : echo "selected";
+                                    endif; ?>>Tidak Aktif</option>
+                                </select>
+                                <small class="text-danger">
+                                    <?php echo form_error('status') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="account_bank" class="col-sm-4 col-form-label">Account Bank</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="account_bank" name="account_bank" value=" <?= set_value('account_bank'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('account_bank') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="rekening_bank" class="col-sm-4 col-form-label">Rekening Bank</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="rekening_bank" name="rekening_bank" value=" <?= set_value('rekening_bank'); ?>">
+                                <small class="text-danger">
+                                    <?php echo form_error('rekening_bank') ?>
+                                </small>
+                            </div>
+                        </div>
+
+                        <div class="form-group row mt-2">
+                            <div class="col-sm-10">
+                                <button type="submit" class="btn btn-primary">Simpan</button>
+                                <a class="btn btn-secondary" href="javascript:history.back()">Kembali</a>
+                            </div>
+                        </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+    </div>
+</div>
